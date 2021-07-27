c6e23853295077dd8cadfa9da90afba9abb10b59
AP <porwal.1@iitj.ac.in>
2021-07-27 13:03:02 +0530

    Minimum Subset diffrence

---
+++ Sorting/Number of Subsequences That Satisfy the Given Sum Condition.py
@@ -0,0 +1,25 @@
+def numSubseq(arr, k):
+    arr.sort()
+    n = len(arr)
+
+    v = [1]
+    for i in range(1, n):
+        v.append((v[i-1]*2) % (10**9+7))
+
+    i = 0
+    j = n-1
+    res = 0
+
+    while(i <= j):
+        if arr[i]+arr[j] <= k:
+            res += v[j-i] % (10**9+7)
+            i += 1
+        else:
+            j -= 1
+
+    return res % (10**9+7)
+
+
+arr = list(map(int, input().split()))
+k = int(input())
+print(numSubseq(arr, k))
