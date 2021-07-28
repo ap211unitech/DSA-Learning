201a083d40111d2953c2ef9446d16b52fb8aee17
AP <porwal.1@iitj.ac.in>
2021-07-29 01:10:37 +0530

    Longest Increasing subsequence

--- Binary Search/Peek Element.py
+++ Binary Search/Peek Element.py
@@ -1,28 +1,3 @@
-# def answer(arr):
-#     n = len(arr)
-#     l = 0
-#     r = n-1
-
-#     while(l <= r):
-#         mid = l+(r-l)//2
-#         if mid > 0 and mid < n-1:
-#             if arr[mid] >= arr[mid-1] and arr[mid] >= arr[mid+1]:
-#                 return mid
-#             if arr[mid-1] > arr[mid]:
-#                 r = mid-1
-#             elif arr[mid+1] > arr[mid]:
-#                 l = mid+1
-#         elif mid == 0:
-#             if arr[mid] > arr[mid+1]:
-#                 return mid  # 0
-#             else:
-#                 return mid+1  # 1
-#         elif mid == n-1:
-#             if arr[mid] > arr[mid-1]:
-#                 return mid # n-1
-#             else:
-#                 return mid-1 # n-2
-
 def answer(arr):
     n = len(arr)
     l = 0
@@ -30,20 +5,47 @@ def answer(arr):
 
     while(l <= r):
         mid = l+(r-l)//2
+        if mid > 0 and mid < n-1:
+            if arr[mid] >= arr[mid-1] and arr[mid] >= arr[mid+1]:
+                return mid
+            if arr[mid-1] > arr[mid]:
+                r = mid-1
+            elif arr[mid+1] > arr[mid]:
+                l = mid+1
+        elif mid == 0:
+            if arr[mid] > arr[mid+1]:
+                return mid  # 0
+            else:
+                return mid+1  # 1
+        elif mid == n-1:
+            if arr[mid] > arr[mid-1]:
+                return mid  # n-1
+            else:
+                return mid-1  # n-2
 
-        if (mid == n-1 or arr[mid] >= arr[mid+1]) and (mid == 0 or arr[mid] >= arr[mid-1]):
-            return arr[mid]
+# def answer(arr):
+#     n = len(arr)
+#     l = 0
+#     r = n-1
 
-        elif mid > 0 and arr[mid] < arr[mid-1]:
-            r = mid-1
+#     while(l <= r):
+#         mid = l+(r-l)//2
 
-        else:
-            l = mid+1
+#         if (mid == n-1 or arr[mid] >= arr[mid+1]) and (mid == 0 or arr[mid] >= arr[mid-1]):
+#             return arr[mid]
 
-    return -1
+#         elif mid > 0 and arr[mid] < arr[mid-1]:
+#             r = mid-1
+
+#         else:
+#             l = mid+1
+
+#     return -1
 
 
 # Input
 arr = list(map(int, input().split()))
-
+if len(arr) <= 1:
+    print(0)
+    exit()
 print(answer(arr))
