19933e333c3a99e01cae9ae8a33b1117615e1e3a
AP <porwal.1@iitj.ac.in>
2021-07-28 15:47:46 +0530

    Longest Palindromic substring

--- Dynamic Programming/LCS/Minimum number of deletions and insertions.py
+++ Dynamic Programming/LCS/Minimum number of deletions and insertions.py
@@ -1,21 +1,21 @@
 def LCS(s, t, n, m):
-
     dp = [[-1 for i in range(m+1)] for j in range(n+1)]
-    res = -float('inf')
 
     for i in range(n+1):
         for j in range(m+1):
-            if i == 0 or j == 0:
+            if i == 0:
+                dp[i][j] = 0
+            elif j == 0:
                 dp[i][j] = 0
             elif s[i-1] == t[j-1]:
                 dp[i][j] = 1+dp[i-1][j-1]
             else:
-                dp[i][j] = 0
-            res = max(dp[i][j], res)
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-    print('Length of longest common substring: ', res)
+    # e.g heap and pea - LCS is ea (2) so , ans is (heap-ea)+(pea-ea)=2+1=3
+    return n+m-2*dp[i][j]
 
 
 s = input()
 t = input()
-LCS(s, t, len(s), len(t))
+print(LCS(s, t, len(s), len(t)))
